c4e77aa46c96ba4ddad0130d5944837f2da0b5b4
Kamaal Farah <kamaal.f1@gmail.com>
2023-08-13 13:11:30 +0200

    Initial commit

---
+++ ObjectiveCPlay/ObjectiveCPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectiveCPlayApp.swift
+//  ObjectiveCPlay
+//
+//  Created by Kamaal M Farah on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObjectiveCPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
